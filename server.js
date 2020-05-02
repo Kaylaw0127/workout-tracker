0b5c6da46ee4d6e829b0afbab316aef660fc8b52
Kayla Ward <kaylaw0127@gmail.com>
2020-05-01 22:29:52 -0700

    trying to add files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./controllers/controller.js");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+const PORT = 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// Routes
+app.use(require("./routes/routes.js"));
+app.use(require("./rouges/html-routes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on: ${PORT}!`);
+});
